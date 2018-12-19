3aecdf2a50072eb99cf10f2ea04b6dc3eee999c4
Rob Moore <rmoore@blockcrushr.com>
2018-12-19 09:23:30 -0400

    Added block counter to empty react app.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
+import Web3 from 'web3';
 import './App.css';
 
+let web3 = new Web3('https://mainnet.infura.io');
+
+
+
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      blockNumber: 0
+    }
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+        () => this.updateInfura(),
+        2000
+    );
+  }
+
+  updateInfura() {
+      return web3.eth.getBlockNumber()
+          .then((resp) => {
+            console.log('checked!');
+            this.setState({
+              blockNumber: resp
+            })
+          });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="container">
+            <h1 className="thinFont center-align orange-text">Constantinople Fork</h1>
+            <h1 className="thinFont center-align"><span id="currentBlock" className="groundhog-green text-accent-2 smallScreen orange-text">{this.state.blockNumber.toLocaleString(navigator.language, { minimumFractionDigits: 0 })}</span> / <span id="forkBlock" className="orange-text smallScreen">7,080,000</span><span >&nbsp;&nbsp;Blocks&nbsp;</span></h1>
+          </div>
         </header>
       </div>
     );
